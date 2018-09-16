f8b7666635d5ffe7d3ace2feff084c80daeec37a
kaduelinho <carlos.libardo@chaordicsystems.com>
2018-09-16 12:38:00 -0300

    Implement cache images and change icon loading/icon button

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
-import { View, Platform, Text } from 'react-native';
+import { View, Platform, Image } from 'react-native';
+import Expo from 'expo';
+import icon from '../assets/icons/pure-icon.png'
 import { STATUS_BAR_HEIGHT } from '../constants';
+
+const cacheImages = images => images.map(image => {
+    if (typeof image === 'string') {
+        return Image.prefetch(image);
+    }
+    return Expo.Asset.fromModule(image).downloadAsync();
+});
 
 class MainScreen extends Component {
     static navigationOptions = () => ({
@@ -10,23 +19,50 @@ class MainScreen extends Component {
             backgroundColor: '#2196F3'
         },
         headerTitleStyle: {
-            marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
+
             color: 'white'
         },
-        headerLeft: <View><Text>I</Text></View>
-    })
+        headerLeft: (
+            <Image
+                source={icon}
+                style={styles.image}
+            >
+            </Image>)
+    });
+
+    state = {
+        AppIsReady: false
+    }
+
+    componentWillMount(){
+        this._loadAssetsAsync();
+    }
+
+    async _loadAssetsAsync() {
+        const imageAssets = cacheImages([icon]);
+        await Promise.all([...imageAssets]);
+        this.setState({AppIsReady: true});
+    }
     render() {
         return (
             <View>
-                <View style={{flex: 1, backgroundColor: '#ddd'}}>
-                   {/* Modal */}
+                <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+                    {/* Modal */}
 
-                   {/* Content */}
+                    {/* Content */}
                 </View>
             </View>
         )
     }
 }
 
+const styles = {
+    image: {
+        marginTop: 7,
+        marginLeft: 10,
+        width: 40,
+        height: 40
+    }
+}
 
 export default MainScreen;
